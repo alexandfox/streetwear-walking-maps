9b1e097e3ad299a836c4287e9d4fde96d3e4229f
alex fox <to.alexfox@gmail.com>
2019-04-22 22:48:43 +0200

    added Create route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
+// const Restaurant = require('../models/restaurant');    --> model example
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/create', (req, res, next) => {
+  res.render('create');
+});
+
+// const createMap = require('./create');
+// app.use('/create', createMap);
 
 module.exports = router;

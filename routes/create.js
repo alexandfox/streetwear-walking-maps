fbe2c211dc288b23ee4b64ae0361b23435550feb
alex fox <to.alexfox@gmail.com>
2019-04-23 14:47:39 +0200

    working place details dialogue

--- routes/create.js
+++ routes/create.js
@@ -6,19 +6,9 @@ const placesAPI = axios.create({
     baseURL: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/'
 });
 
-var placeSearch = {
-  key: "AIzaSyBAFajUxQ7Ltv5t9nfiaYTXvhnWbTV80bk",
-  input: "opera-house",
-  inputetype: "textquery"
-}
 
-function getPlaceInfo( placeSearch ) {
-  restCountriesApi.get( 'json', placeSearch)
-    .then( responseFromAPI => {
-        console.log('Response from API is: ', responseFromAPI.data);           
-})		.catch(err => {
-    console.log('Error is: ', err);
-})};
+
+
 
 
 // router.get('/create', (req, res, next) => {

d229df01827c5f31015946065a7ca113e32e2d21
alex fox <to.alexfox@gmail.com>
2019-04-24 16:34:44 +0200

    map view working

--- models/map.js
+++ models/map.js
@@ -38,6 +38,13 @@ var japanMap = new Map({
 		destination: {'placeId': "ChIJU9Ydb2GLGGAR5UIVmYONRiU"},
 		travelMode: "WALKING",
 		waypoints: [
+			{location: {'placeId': "ChIJHTQ2_qWMGGARK_lj8y-fYRE"}},
+			{location: {'placeId': "ChIJCzp6MKGMGGARHw84njJix8c"}},
+			{location: {'placeId': "ChIJoQkPWaGMGGARKKaniscvhas"}},
+			{location: {'placeId': "ChIJ9WtFk6SMGGARVuEQhQtcrcw"}},
+			{location: {'placeId': "ChIJzdgvf6OMGGARnxNSOWekExE"}},
+			{location: {'placeId': "ChIJ83ZdS2CLGGAREqhGUhmJ9pM"}},
+			{location: {'placeId': "ChIJlarnCmGLGGARJSZpRnbZg4c"}},
 		],
 		optimizeWaypoints: false,
 		provideRouteAlternatives: false,
@@ -69,21 +76,21 @@ var japanMap = new Map({
 //   .then(map => { console.log('The map was created: ', map) })
 //   .catch(err => { console.log('An error occured', err) });
 
-Map.findById('5cc04c3bfb7fa611209ceb6c')
-  .then( map => {
-    map.map.waypoints = [
-			{'placeId': "ChIJHTQ2_qWMGGARK_lj8y-fYRE"},
-			{'placeId': "ChIJCzp6MKGMGGARHw84njJix8c"},
-			{'placeId': "ChIJoQkPWaGMGGARKKaniscvhas"},
-			{'placeId': "ChIJ9WtFk6SMGGARVuEQhQtcrcw"},
-			{'placeId': "ChIJzdgvf6OMGGARnxNSOWekExE"},
-			{'placeId': "ChIJ83ZdS2CLGGAREqhGUhmJ9pM"},
-			{'placeId': "ChIJlarnCmGLGGARJSZpRnbZg4c"},
-		]
-    return map.save(); 
-  })
-  .then(map => { console.log('The map was updated: ' + map )})
-  .catch(err => { console.log('An error occured:', err) });
+// Map.findByIdAndUpdate('5cc04c3bfb7fa611209ceb6c')
+//   .then( map => {
+//     map.map.waypoints = [
+// 			{'placeId': "ChIJHTQ2_qWMGGARK_lj8y-fYRE"},
+// 			{'placeId': "ChIJCzp6MKGMGGARHw84njJix8c"},
+// 			{'placeId': "ChIJoQkPWaGMGGARKKaniscvhas"},
+// 			{'placeId': "ChIJ9WtFk6SMGGARVuEQhQtcrcw"},
+// 			{'placeId': "ChIJzdgvf6OMGGARnxNSOWekExE"},
+// 			{'placeId': "ChIJ83ZdS2CLGGAREqhGUhmJ9pM"},
+// 			{'placeId': "ChIJlarnCmGLGGARJSZpRnbZg4c"},
+// 		]
+//     return map.save(); 
+//   })
+//   .then(map => { console.log('The map was updated: ' + map )})
+//   .catch(err => { console.log('An error occured:', err) });
 
 
 
